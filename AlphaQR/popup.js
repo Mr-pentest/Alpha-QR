76f67dbcdb4c392773cf835126d94834222a0011
Joseph <josephwebso@gmail.com>
2025-11-24 23:43:40 +0530

    popup.js

--- AlphaQR/popup.js
+++ AlphaQR/popup.js
@@ -18,7 +18,7 @@ function updateUI(state, tabId) {
   const icon = document.getElementById("statusIcon");
   const text = document.getElementById("statusText");
 
-  icon.src = "icons/AlphaQR.png";
+  icon.src = "Icons/AlphaQR.png";
   text.innerText = state === "running" ? "Running" : "Stopped";
 
   document.getElementById("tabId").innerText = tabId;
